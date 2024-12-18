eecc0e3a5a59d308f49eb29ba7b4da39affaa0d0
Felicia Dahl <feliciadahl01@gmail.com>
2024-12-18 15:38:52 +0100

    Add project files.

---
+++ MainApp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using MainApp.ViewModels;
+using MainApp.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace MainApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                 .ConfigureServices(services =>
+                 {
+                     services.AddSingleton<MainViewModel>();
+                     services.AddSingleton<MainWindow>();
+
+                     services.AddTransient<ContactViewModel>();
+                     services.AddTransient<ContactsView>();
+
+                     services.AddTransient<AddContactViewModel>();
+                     services.AddTransient<AddContactView>();
+
+                 })
+                 .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ MainApp/ViewModels/ContactViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MainApp.ViewModels;
+
+public partial class ContactViewModel : ObservableObject
+{
+
+    private readonly IServiceProvider _serviceProvider;
+
+    [ObservableProperty]
+    private string _title = "Contacts";
+
+  
+
+    [RelayCommand]
+    private void GoToAddContact()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _serviceProvider.GetRequiredService<AddContactViewModel>();
+    }
+
+    public ContactViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+}
